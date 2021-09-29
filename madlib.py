b04eaceacf47ca37be8b6dc69353cd2f0c55acf8
Suvd Tod <t.suvdaa0424@gmail.com>
2021-09-29 00:44:09 -0400

    added madlib.py

---
+++ madlib.py
@@ -0,0 +1,6 @@
+print('''Please fill in the blanks below:
+____(name)____'s favorite subject in school is ____(subject)____.''')
+name = input('What is name? ')
+subject = input('What is subject? ')
+
+print(f"{name}'s favorite subject in school is {subject}.")
